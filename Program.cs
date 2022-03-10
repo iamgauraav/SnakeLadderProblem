54e53cc4dcc9164190f87fcb8334cf543411c5d4
iamgauraav <iamgauraav@gmail.com>
2022-03-10 21:53:55 +0530

    Added welcome message to master branch

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake and ladder problem on master branch ");
+            Console.ReadLine();
+        }
+    }
+}
