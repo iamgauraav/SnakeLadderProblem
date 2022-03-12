1d249536279445cec0857e706afa8ba9af874ced
iamgauraav <iamgauraav@gmail.com>
2022-03-12 13:27:30 +0530

    Refactoring code

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 
 namespace SnakeLadder
-{
+{/// <summary>
+ /// UC6- Report number of times dice was played to win and the position after every die rolled
+ /// </summary>
 
     class Program
     {
@@ -9,6 +11,7 @@ namespace SnakeLadder
         public const int LADDER = 1;
         public const int SNAKE = 2;
         public const int NO_PLAY = 3;
+
 
         //Method to return CheckDice
         public static int CheckDice()
@@ -21,88 +24,54 @@ namespace SnakeLadder
 
         public static void CheckOption()
         {
-            
+            int position = 0;
+            int numOfDiceRolles = 0;
             Random random = new Random();           //random method to generate random number
             int CheckOption = random.Next(1, 4);    //next method to generate value from 1 to 3
             int CheckDie = Program.CheckDice();     //Called the CheckDice in which position 1 to 6 is stored
-            int currentPlayer = 1;
-            int posPlayer = 0;
-            int posplayer1 = 0;
-            int posplayer2 = 0;
-            int numOfDiceRolles = 0;
+
+
 
             //while loop
-            while (posPlayer < 100 && posplayer2 <= 100)
+            while (position < 100)
             {
-                numOfDiceRolles++;
+                numOfDiceRolles++;                  //increase the no. of time dice rolls
                 //If else if selection statement
-
-                //re-initilize variable
-                if (currentPlayer == 1)
-                {
-                    posPlayer = posplayer1;
-                }
-                else
-                {
-                    posPlayer = posplayer2;
-                }
-
                 if (CheckOption == LADDER)
                 {
-                    if (posPlayer + CheckDie <= 100) //Checking if the dice goes above 100 than it come to 100 position
+                    if (position + CheckDie <= 100) //Checking if the dice goes above 100 than it come to 100 position
                     {
-                        posPlayer += CheckDie;
+                        position += CheckDie;
                         Console.WriteLine("LADDER");
-                        Console.WriteLine("player current position : " + posPlayer);
+                        Console.WriteLine("player current position : " + position);
                     }
                     else
                     {
-                        posPlayer++;
+
                         Console.WriteLine("LADDER");
-                        Console.WriteLine("player current position : " + posPlayer);
+                        Console.WriteLine("player current position : " + position);
                     }
 
                 }
                 else if (CheckOption == SNAKE)
                 {
-                    if (posPlayer - CheckDie <= 0)   //Checking if the dice goes below zero than come at starting position
+                    if (position - CheckDie <= 0)   //Checking if the dice goes below zero than come at starting position
                     {
-                        posPlayer = 0;
+                        position = 0;
                     }
                     else
                     {
-                        posPlayer -= CheckDie;
+                        position -= CheckDie;
                         Console.WriteLine("SNAKE");
-                        Console.WriteLine("player current position : " + posPlayer);
+                        Console.WriteLine("player current position : " + position);
                     }
 
                 }
                 else
                 {
                     Console.WriteLine("NO Play");
-                    posPlayer++;
                 }
-                // re-assign return variable
-                if (currentPlayer == 1)
-                {
-                    posplayer1 = posPlayer;
-                }
-                else
-                {
-                    posplayer2 = posPlayer;
-                }
-
-                //Checking winner
-                if (posplayer1 == 100)
-                {
-                    Console.WriteLine("player one winner");
-                }
-                else if(posplayer2 == 100)
-                {
-                    Console.WriteLine("player two is winner");
-                }
-                Console.WriteLine("number of rolls die"+ numOfDiceRolles);
-
+                Console.WriteLine("Number of dice Rolles: " + numOfDiceRolles); //No. to times dice rolled
             }
 
         }
